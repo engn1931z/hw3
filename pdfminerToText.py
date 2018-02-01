0a507e32d81e84e9613bb7a779f173f31383494f
matthew-hsr <shao_ran_huang@brown.edu>
2018-02-01 13:06:09 -0500

    Update pdfminerToText.py

--- pdfminerToText.py
+++ pdfminerToText.py
@@ -1,6 +1,8 @@
 # Following code from https://www.binpress.com/tutorial/manipulating-pdfs-with-python/167
 # Tim Arnold article on "Manipulating PDFs from Python)
 
+# Using PDFMiner.six
+
 import io
 
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
@@ -14,20 +16,20 @@ def convert(fname, pages=None):
         pagenums = set()
     else:
         pagenums = set(pages)
-    rsrcmgr = PDFResourceManager()
-    retstr = io.StringIO()
-    device = TextConverter(rsrcmgr, retstr, codec='utf-8', laparams=LAParams())
-    fp = open(fname, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    for page in PDFPage.get_pages(fp, pagenums, maxpages=0,
+    output = io.StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, codec='utf-8', laparams=LAParams())
+    infile = open(fname, 'rb')
+    interpreter = PDFPageInterpreter(manager, converter)
+    for page in PDFPage.get_pages(infile, pagenums, maxpages=0,
                                   password="",
                                   caching=True,
                                   check_extractable=True):
         interpreter.process_page(page)
-    text = retstr.getvalue()
-    fp.close()
-    device.close()
-    retstr.close()
+    text = output.getvalue()
+    infile.close()
+    converter.close()
+    output.close()
     return text
 
 # Note: For alternative implementations, see http://stackoverflow.com/questions/26494211/extracting-text-from-a-pdf-file-using-pdfminer-in-python/
