8001a7e05186a88e171ca5b7238ca665f18d27b5
matthew-hsr <shao_ran_huang@brown.edu>
2018-02-01 11:05:49 -0500

    Update pdfminerToText.py

--- pdfminerToText.py
+++ pdfminerToText.py
@@ -1,30 +1,33 @@
 # Following code from https://www.binpress.com/tutorial/manipulating-pdfs-with-python/167
 # Tim Arnold article on "Manipulating PDFs from Python)
 
-from io import StringIO
+import io
+
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 
+
 def convert(fname, pages=None):
-    if not pages:
+    if not pages:   
         pagenums = set()
     else:
         pagenums = set(pages)
-
-    output = StringIO()
-    manager = PDFResourceManager()
-    converter = TextConverter(manager, output, laparams=LAParams())
-    interpreter = PDFPageInterpreter(manager, converter)
-
-    infile = open(fname, 'rb')
-    for page in PDFPage.get_pages(infile, pagenums):
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    device = TextConverter(rsrcmgr, retstr, codec='utf-8', laparams=LAParams())
+    fp = open(fname, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.get_pages(fp, pagenums, maxpages=0,
+                                  password="",
+                                  caching=True,
+                                  check_extractable=True):
         interpreter.process_page(page)
-    infile.close()
-    converter.close()
-    text = output.getvalue()
-    output.close
+    text = retstr.getvalue()
+    fp.close()
+    device.close()
+    retstr.close()
     return text
 
 # Note: For alternative implementations, see http://stackoverflow.com/questions/26494211/extracting-text-from-a-pdf-file-using-pdfminer-in-python/
